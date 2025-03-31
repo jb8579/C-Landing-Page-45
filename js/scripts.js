7eb075d8dc283987fced0955154659a6b98cde62
jb8579 <jake.brown8579@gmail.com>
2025-03-31 22:31:53 +0600

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+(function($){
+	$(document).ready(function() {	
+
+     
+   
+
+
+
+
+	    //mobile nav menu
+    jQuery(".menu-icon").click(function() {
+    	jQuery(".Close-icon").show();
+    });
+
+    jQuery(".menu-icon").click(function() {
+    	jQuery(".menu-icon").hide();
+    });
+
+
+     jQuery(".Close-icon").click(function() {
+    	jQuery(".menu-icon").show();
+    });
+
+    jQuery(".Close-icon").click(function() {
+    	jQuery(".Close-icon").hide();
+    });
+
+
+
+    jQuery(".menu-part img").click(function() {
+    	jQuery(".header-btns").slideToggle();
+    });
+
+
+
+
+
+
+
+
+
+		
+
+				
+
+		
+	});
+})(jQuery);
